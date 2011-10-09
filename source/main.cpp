cb55b0fc5990b6e99a25fd5779b5490d750ba367
Fabian Schuiki <fabianschuiki@axamblis.com>
2011-10-09 12:57:30 +0200

    Reverted the actual Maxwell compiler code to main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -1,23 +1,8 @@
 #include <iostream>
 #include <fstream>
 
-#include "driver.h"
-
-using namespace Maxwell;
-
 
 int main(int argc, char * argv[])
 {
-	/*std::ifstream fin("../examples/class.mw");
-	FlexLexer * lexer = new yyFlexLexer(&fin);
-	while (int result = lexer->yylex())
-		std::cout << "result = " << result << "\n";*/
-	
-	//Create a new driver to read stuff.
-	Driver driver;
-	driver.parse("debug.mw");
-	if (driver.programBlock)
-		std::cout << driver.programBlock->describe();
-	
 	return 0;
 }

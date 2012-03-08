ff489239531b14962dd26c9efe145ca9db0cf593
Fabian Schuiki <fabianschuiki@axamblis.com>
2012-03-08 11:23:34 +0100

    Added UnitTesting framework

---
+++ language/php/bin/test.types.php
@@ -0,0 +1,6 @@
+#!/usr/bin/php
+<?php
+require_once __DIR__.'/../lib/autoload.php';
+
+UnitTest::test('{int}', '{int}');
+UnitTest::test('int', '{int}');
---
+++ language/php/lib/UnitTest.php
@@ -0,0 +1,21 @@
+<?php
+
+class UnitTest
+{
+	static public function test($result, $desired)
+	{
+		if ($result != $desired) {
+			$trace = debug_backtrace();
+			$entry = array_shift($trace);
+			$filename = basename($entry['file']);
+			$lines = file($entry['file']);
+			$linenum = $entry['line'];
+			$line = $lines[$linenum-1];
+			preg_match('/^.*?\((.*?)\);\s*$/', $line, $matches);
+			$line = $matches[1];
+			echo "- $desired: \033[31mFAILED\033[0m, got \033[36m$result\033[0m\n  -> $filename.$linenum: $line\n";
+		} else {
+			echo "- $desired: \033[32mpassed\033[0m\n";
+		}
+	}
+}

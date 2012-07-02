04d8f0fbe976ac2ebabd99f6d2e5e924d2555810
Fabian Schuiki <fabianschuiki@axamblis.com>
2012-07-02 22:45:04 +0200

    Added some new ideas and concepts.

--- concept/Ideas.js
+++ concept/Ideas.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
 	$("code").each(function(){
 		var code = $(this).html();
 		
-		code = code.replace(/(\"[^\"]*\")/g, "<span class=\"string\">$1</span>");
+		code = code.replace(/(^|[^=])(\"[^\"]*\")/g, "$1<span class=\"string\">$2</span>");
 		code = code.replace(/([^\w])(\.?\d[\d\.]*)/g, "$1<span class=\"number\">$2</span>");
 		code = code.replace(/(var\s+|isa\s+)([\w|]+)/g, "$1<span class=\"type\">$2</span>");
 		code = code.replace(/(\w+)\(/g, "<span class=\"function\">$1</span>(");
-		code = code.replace(/(^|\s)(var|if|else|isa|func|type)(\s|$)/g, "$1<span class=\"keyword\">$2</span>$3");
+		code = code.replace(/(^|[^\w\"])(var|if|else|isa|func|type|return)([^\w\"]|$)/g, "$1<span class=\"keyword\">$2</span>$3");
 		code = code.replace(/(\/\/.*?$|\/\*.*?\*\/)/mg, "<span class=\"comment\">$1</span>");
 		
 		$(this).html(code);

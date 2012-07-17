340cd2b93425a9ea842cf20cb2bfa6e9332f93ea
Fabian Schuiki <fabianschuiki@axamblis.com>
2012-07-17 17:48:52 +0200

    Added two new ideas.

--- concept/include/include.js
+++ concept/include/include.js
@@ -6,7 +6,8 @@ $(document).ready(function(){
 		code = code.replace(/([^\w])(\.?\d[\d\.]*)/g, "$1<span class=\"number\">$2</span>");
 		code = code.replace(/(var\s+|isa\s+)([\w|]+)/g, "$1<span class=\"type\">$2</span>");
 		code = code.replace(/(\w+)\(/g, "<span class=\"function\">$1</span>(");
-		code = code.replace(/(^|[^\w\"])(var|if|else|isa|func|type|return)([^\w\"]|$)/g, "$1<span class=\"keyword\">$2</span>$3");
+		code = code.replace(/(^|[^\w\"])(null|nil|true|false)([^\w\"]|$)/g, "$1<span class=\"constant\">$2</span>$3");
+		code = code.replace(/(^|[^\w\"])(var|if|else|isa|func|type|return|for|incase|otherwise)([^\w\"]|$)/g, "$1<span class=\"keyword\">$2</span>$3");
 		code = code.replace(/(\/\/.*?$|\/\*.*?\*\/)/mg, "<span class=\"comment\">$1</span>");
 		
 		$(this).html(code);

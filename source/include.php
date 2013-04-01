df49f3bdadd80a54303b2f863611ac722da49f72
Fabian Schuiki <fabian@schuiki.ch>
2013-04-02 00:33:16 +0200

    Added better exception handler.

--- source/include.php
+++ source/include.php
@@ -7,3 +7,19 @@ function vartype($v)
 		return get_class($v);
 	return gettype($v);
 }
+
+function exception_handler($exception)
+{
+	echo "\n";
+	echo "\033[31;1m*** EXCEPTION ***\033[0m (".get_class($exception).")\n";
+	echo "Message: \033[1m{$exception->getMessage()}\033[0m\n";
+	echo "File: \033[36;1m".str_replace(__DIR__."/", "", $exception->getFile())."\033[0m line \033[36m".$exception->getLine()."\033[0m\n";
+	echo "\n";
+
+	// Print the backtrace.
+	echo str_replace(__DIR__."/", "", $exception->getTraceAsString())."\n";
+	echo "\n";
+
+	exit(1);
+}
+set_exception_handler("exception_handler");

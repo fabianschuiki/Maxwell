d39683f9550c85b91b72a9ebb1f625650ab3eddc
Fabian Schuiki <fabian@schuiki.ch>
2013-04-06 11:59:04 +0200

    Added some annotations.

--- source/PrettyPrinter.php
+++ source/PrettyPrinter.php
@@ -15,34 +15,59 @@ class PrettyPrinter
 		return "    ".str_replace("\n", "\n    ", $s);
 	}
 
-	static private function precedenceWrap($s, $context, $precedence)
+	static private function precedenceWrap($s, &$context, $precedence)
 	{
-		$outer = @$context["precedence"];
+		if (!isset($context["precedence"]))
+			return $s;
+		$outer = $context["precedence"];
 		if (!$outer || $outer <= $precedence)
 			return $s;
 		return "($s)";
+	}
+
+	static private function ctxget(&$context, $path)
+	{
+		$obj = $context;
+		foreach (explode("/", $path) as $index) {
+			if (!isset($obj[$index]))
+				return null;
+			$obj = $obj[$index];
+		}
+		return $obj;
 	}
 
 	/**
 	 * Returns a string containing valid Maxwell code for the given root object
 	 * ID.
 	 */
-	public function formatRootObject($objectId)
+	public function formatRootObject($objectId, &$context = null)
 	{
 		$object = $this->repository->getObject($objectId);
-		return $this->formatObject($object);
+		return $this->formatObject($object, $context);
 	}
 
-	public function formatObject(RepositoryObject $object, $context = null)
+	public function formatObject(RepositoryObject $object, &$context = null)
 	{
 		$funcName = "format".$object->getClass();
 		if (method_exists($this, $funcName)) {
+			if ($object instanceof \Objects\CallInterface && static::ctxget($context, "annotate/selected-call") === true) {
+				$c = $object->getSelectedCallCandidate(true);
+				if ($c) {
+					$context["annotations"][] = "{$object->getCallName()} calls {$c->getFunc()->getId()}";
+				}
+			}
+			if ($object instanceof \Objects\TypeInterface) {
+				if (static::ctxget($context, "annotate/types") === true) {
+					$at = $object->getActualType();
+					$context["annotations"][] = "type of {$object->getId()} = ".\Type::describe($at);
+				}
+			}
 			return $this->$funcName($object, $context);
 		}
 		throw new \InvalidArgumentException("Don't know how to pretty-print {$object->getClass()}.");
 	}
 
-	public function formatFunctionDefinition($object, $context)
+	public function formatFunctionDefinition($object, &$context)
 	{
 		$inputs = array();
 		$outputs = array();
@@ -68,17 +93,27 @@ class PrettyPrinter
 		return $s;
 	}
 
-	public function formatFunctionArgument($object, $context)
+	public function formatFunctionArgument($object, &$context)
 	{
 		return $this->formatObject($object->getTypeExpr(), $context)." ".$object->getName();
 	}
 
-	public function formatExprStmt($object, $context)
+	public function formatExprStmt($object, &$context)
 	{
-		return $this->formatObject($object->getExpr(), $context).";";
+		unset($context["annotations"]);
+		$s = $this->formatObject($object->getExpr(), $context).";";
+		$ans = static::ctxget($context, "annotations");
+		unset($context["annotations"]);
+		if ($ans) {
+			foreach ($ans as $an) {
+				$anmod = str_replace($object->getId().".", "", $an);
+				$s .= "\n// ".str_replace("\n", "\n// ", $anmod);
+			}
+		}
+		return $s;
 	}
 
-	public function formatAssignmentExpr($object, $context)
+	public function formatAssignmentExpr($object, &$context)
 	{
 		$precedence = 0;
 		$s  = $this->formatObject($object->getLhs(), $context);
@@ -87,7 +122,7 @@ class PrettyPrinter
 		return static::precedenceWrap($s, $context, $precedence);
 	}
 
-	public function formatBinaryOperatorExpr($object, $context)
+	public function formatBinaryOperatorExpr($object, &$context)
 	{
 		$precedence = 1;
 		if (in_array($object->getOperator(), array("*", "/")))
@@ -98,18 +133,27 @@ class PrettyPrinter
 		return static::precedenceWrap($s, $context, $precedence);
 	}
 
-	public function formatIdentifierExpr($object, $context)
+	public function formatIdentifierExpr($object, &$context)
 	{
 		return $object->getName();
 	}
 
-	public function formatConstantExpr($object, $context)
+	public function formatConstantExpr($object, &$context)
 	{
 		return $object->getValue();
 	}
 
-	public function formatTypeExpr($object, $context)
+	public function formatTypeExpr($object, &$context)
 	{
 		return $this->formatObject($object->getExpr(), $context);
+	}
+
+	public function formatCallExpr($object, &$context)
+	{
+		$args = array();
+		foreach ($object->getArguments()->getElements() as $arg) {
+			$args[] = $this->formatObject($arg, $context);
+		}
+		return $object->getName()."(".implode(", ", $args).")";
 	}
 }
--- tests/driver.pretty.php
+++ tests/driver.pretty.php
@@ -7,6 +7,16 @@ $repo = new Repository(__DIR__."/repo");
 // Initialize the pretty printer.
 $printer = new PrettyPrinter($repo);
 
+// Assemble a context.
+$ctx = array();
+$ctx["annotate"]["selected-call"] = true;
+$ctx["annotate"]["types"] = true;
+
 // Generate and store the output.
 $path = __DIR__."/repo/pretty.mw";
-file_put_contents($path, $printer->formatRootObject("1.1"));
+$output = "";
+foreach (array("1.1", "1.2") as $id) {
+	$output .= "// $id\n";
+	$output .= $printer->formatRootObject($id, $ctx)."\n\n";
+}
+file_put_contents($path, trim($output));

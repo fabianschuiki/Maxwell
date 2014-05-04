8589417cfa64de85e8805f98747a2e06cfadebd5
Fabian Schuiki <fschuiki@student.ethz.ch>
2014-05-04 12:07:51 +0200

    started adding dependency fetch from db

--- cpp/source/backend/c/Packager.cpp
+++ cpp/source/backend/c/Packager.cpp
@@ -18,11 +18,17 @@ void Packager::run(const std::string& name)
 {
 	int rc;
 
-	// Prepare a SQL statement that fetches a fragment from the database.
-	sqlite3_stmt *stmt;
-	rc = sqlite3_prepare_v2(db, "SELECT code,ref FROM fragments WHERE name = ?", -1, &stmt, NULL);
+	// Prepare a SQL statement that fetches a fragment from the database, and
+	// one that fetches all dependencies.
+	sqlite3_stmt *fragStmt, *depsStmt;
+
+	rc = sqlite3_prepare_v2(db, "SELECT rowid,code,ref FROM fragments WHERE name = ?", -1, &fragStmt, NULL);
 	if (rc != SQLITE_OK)
 		throw sqlite3_exception(rc, "Unable to prepare fragment fetch statement");
+
+	rc = sqlite3_prepare_v2(db, "SELECT name FROM dependencies JOIN fragments ON fragments.rowid = depid WHERE fragid = ?", -1, &depsStmt, NULL);
+	if (rc != SQLITE_OK)
+		throw sqlite3_exception(rc, "Unable to prepare dependency fetch statement");
 
 	// Create the queue that holds the fragment names that need to be gathered
 	// from the database.
@@ -37,34 +43,51 @@ void Packager::run(const std::string& name)
 		names.pop();
 
 		// Bind the first parameter to the name of the fragment.
-		rc = sqlite3_bind_text(stmt, 1, n.c_str(), -1, SQLITE_STATIC);
+		rc = sqlite3_bind_text(fragStmt, 1, n.c_str(), -1, SQLITE_STATIC);
 		if (rc != SQLITE_OK)
 			throw sqlite3_exception(rc, "Unable to bind name in fragment fetch statement");
 
 		// Fetch the result from the database. If sqlite3_step() returns DONE,
 		// nothing was found in the database and an error should be returned.
-		rc = sqlite3_step(stmt);
+		rc = sqlite3_step(fragStmt);
 		if (rc == SQLITE_DONE)
 			throw std::runtime_error("No fragment named '" + n + "' found in the database");
 		else if (rc != SQLITE_ROW)
 			throw sqlite3_exception(rc, "Unable to step fragment fetch statement");
 
 		// Create the fragment from the table row.
-		std::string frag_code = (const char*)sqlite3_column_text(stmt, 0);
-		std::string frag_ref  = (const char*)sqlite3_column_text(stmt, 1);
-		std::cout << "[fragment " << n << "] ref = '" << frag_ref << "', code = " << frag_code << '\n';
+		sqlite3_int64 fragRowId = sqlite3_column_int64(fragStmt, 0);
+		std::string   fragCode  = (const char*)sqlite3_column_text(fragStmt, 1);
+		std::string   fragRef   = (const char*)sqlite3_column_text(fragStmt, 2);
+		std::cout << "[fragment #" << fragRowId << " " << n << "] ref = '" << fragRef << "', code = " << fragCode << '\n';
 
-		// Reset the statement such that it may be reused in the next loop
+		
+
+		// Bind the fragment rowid to the first argument of the dependency
+		// statement.
+		rc = sqlite3_bind_int64(depsStmt, 1, fragRowId);
+		if (rc != SQLITE_OK)
+			throw sqlite3_exception(rc, "Unable to bind fragment rowid in dependency fetch statement");
+
+		// Reset the statements such that it may be reused in the next loop
 		// iteration.
-		rc = sqlite3_reset(stmt);
+		rc = sqlite3_reset(fragStmt);
 		if (rc != SQLITE_OK)
 			throw sqlite3_exception(rc, "Unable to reset fragment fetch statement");
+
+		rc = sqlite3_reset(depsStmt);
+		if (rc != SQLITE_OK)
+			throw sqlite3_exception(rc, "Unable to reset dependency fetch statement");
 
 		std::cout << "gathering " << n << '\n';
 	}
 
-	// Dispose of the statement.
-	rc = sqlite3_finalize(stmt);
+	// Dispose of the statements.
+	rc = sqlite3_finalize(fragStmt);
 	if (rc != SQLITE_OK)
 		throw sqlite3_exception(rc, "Unable to finalize fragment fetch statement");
+
+	rc = sqlite3_finalize(depsStmt);
+	if (rc != SQLITE_OK)
+		throw sqlite3_exception(rc, "Unable to finalize dependency fetch statement");
 }
--- cpp/source/sqlite3.hpp
+++ cpp/source/sqlite3.hpp
@@ -15,14 +15,16 @@ struct sqlite3_exception : public std::exception
 {
 	int err;
 	std::string comment, errmsg;
+	std::string msg;
 
 	sqlite3_exception(int err, const std::string& comment) throw(): err(err), comment(comment) {
 		errmsg = sqlite3_errstr(err);
+		std::stringstream s;
+		s << comment << "; " << errmsg << " (sqlite3 error " << err << ')';
+		msg = s.str();
 	}
 	virtual ~sqlite3_exception() throw() {}
 	virtual const char* what() const throw() {
-		std::stringstream s;
-		s << comment << "; " << errmsg << " (sqlite3 error " << err << ')';
-		return s.str().c_str();
+		return msg.c_str();
 	}
 };

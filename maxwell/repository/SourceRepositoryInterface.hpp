98bab9f5d10bfe2811e9df4e50d4b4c4eac0e539
Fabian Schuiki <fschuiki@student.ethz.ch>
2014-09-23 22:32:44 +0200

    reworked src repo intf
    Added proper documentation for the interface of a source repository. The file
    SourceRepositoryInterface.hpp will be renamed to SourceRepository.hpp soon. A
    concrete subclass of said interface shall be created that implements the repo
    functionality using the file system. An additional subclass might be devised
    that performs this task using a sqlite3 database.

--- maxwell/repository/SourceRepositoryInterface.hpp
+++ maxwell/repository/SourceRepositoryInterface.hpp
@@ -1,16 +1,61 @@
 /* Copyright (c) 2014 Fabian Schuiki */
 #pragma once
-#include "maxwell/filesystem.hpp"
+#include "maxwell/location.hpp"
 
 namespace maxwell {
 namespace repository {
 
-class SourceRepositoryInterface {
-public:
-	virtual const Path& getPath() const = 0;
+/// An object that maintains a set of source files and detects changes in them.
+/// For each source file added, the repository must track
+///
+/// - a unique id (the source id),
+/// - the file's path,
+/// - contents,
+/// - hash of the contents, and
+/// - modification timestamp.
+///
+/// Adding a file to the repository and updating an existing file's entry is the
+/// same operation. To do this, the repository requires a data structure that
+/// represents a file and gives access to the file's path, modification
+/// timestamp and contents.
+///
+/// Removing a file from the repository requires knowledge of that file's path
+/// or unique id.
+///
+/// A file can be requested from the repository given its path or unique id. The
+/// list of files may be accessed directly as an opaque, iterable field; or by
+/// obtaining a begin and end iterator as is common in C++.
 
-	virtual bool addFile(const Path& path) = 0;
-	virtual bool removeFile(const Path& path) = 0;
+class SourceRepository {
+public:
+
+	/// Adds the given \a file to the repository under the given \a path. If the
+	/// repository already contains an entry for the \a path, that entry is
+	/// updated.
+	///
+	/// \note Note that the separate \a path argument allows for the paths in
+	/// the repository to be relative to some common location, e.g. the root of
+	/// the source base. By contrast, the \a file is likely to have a path
+	/// relative to the current working directory.
+	///
+	/// \return Returns false if the file was already present in the repository
+	/// with the same contents as the \a file provided. Otherwise returns true
+	/// if an entry in the repository was updated or changed.
+	virtual bool add(const Path& path, const File& file) = 0;
+
+	/// Removes the source file with the given \a sid.
+	/// \return Returns whether the file was present in the repository and had
+	/// to be removed.
+	virtual bool remove(SourceId sid) = 0;
+
+	/// Removes the source file with the given \a path. Note that this is the
+	/// path that was passed to the add() function as \a path argument.
+	/// \return Returns whether the file was present in the repository and had
+	/// to be removed.
+	virtual bool remove(const Path& path) = 0;
+
+	/// \todo Add accessor to individual files.
+	/// \todo Add iterator over all files.
 };
 
 } // namespace repository

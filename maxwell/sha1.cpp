5b75c9fad2d9c2d11b8fac396829d3f6ae26bb82
Fabian Schuiki <fschuiki@student.ethz.ch>
2014-10-04 16:53:37 +0200

    fixed sha1 fromhex bug
    Increased the unit test coverage of sha1. This revealed a bug where fromhex()
    would not properly parse the hex string due to an offset error (a..f having
    values 0..5 instead of 10..15).

--- maxwell/sha1.cpp
+++ maxwell/sha1.cpp
@@ -41,9 +41,9 @@ bool sha1hash::fromhex(const std::string& hex, sha1hash& out) {
 		if (c >= '0' && c <= '9') {
 			return c - '0';
 		} else if (c >= 'a' && c <= 'f') {
-			return c - 'a';
+			return c - 'a' + 10;
 		} else if (c >= 'A' && c <= 'F') {
-			return c - 'A';
+			return c - 'A' + 10;
 		} else {
 			return -1;
 		}
--- test/sha1.cpp
+++ test/sha1.cpp
@@ -3,17 +3,43 @@
 #include "maxwell/sha1.hpp"
 #include <boost/test/unit_test.hpp>
 using maxwell::sha1;
+using maxwell::sha1hash;
 
 BOOST_AUTO_TEST_CASE(sha1_case) {
 	// FIPS 180-2 C.1 and RFC3174 7.3 TEST1
-	BOOST_CHECK_EQUAL(sha1().update("abc").finalize().lhex(), "a9993e364706816aba3e25717850c26c9cd0d89d");
+	auto t1 = sha1().update("abc").finalize();
+	BOOST_CHECK_EQUAL(t1.lhex(), "a9993e364706816aba3e25717850c26c9cd0d89d");
+	BOOST_CHECK_EQUAL(t1.uhex(), "A9993E364706816ABA3E25717850C26C9CD0D89D");
+
+	sha1hash t1b, t1c;
+	BOOST_CHECK(!sha1hash::fromhex("axxxxe364706816aba3e25717850c26c9cd0d89d", t1b));
+	BOOST_CHECK(!sha1hash::fromhex("whadup", t1b));
+	BOOST_CHECK(sha1hash::fromhex("a9993e364706816aba3e25717850c26c9cd0d89d", t1b));
+	BOOST_CHECK(sha1hash::fromhex("A9993E364706816ABA3E25717850C26C9CD0D89D", t1c));
+	BOOST_CHECK_EQUAL(t1b, t1c);
+	BOOST_CHECK_EQUAL(t1, t1b);
+	BOOST_CHECK_EQUAL(t1, t1c);
 
 	// FIPS 180-2 C.2 and RFC3174 7.3 TEST2
-	BOOST_CHECK_EQUAL(sha1().update("abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq").finalize().lhex(), "84983e441c3bd26ebaae4aa1f95129e5e54670f1");
+	auto t2 = sha1().update("abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq").finalize();
+	BOOST_CHECK_EQUAL(t2.lhex(), "84983e441c3bd26ebaae4aa1f95129e5e54670f1");
+	BOOST_CHECK_EQUAL(t2.uhex(), "84983E441C3BD26EBAAE4AA1F95129E5E54670F1");
 
 	// RFC3174 7.3 TEST4
-	sha1 t4;
+	sha1 a4;
 	for (unsigned i = 0; i < 80; i++)
-		t4.update("01234567");
-	BOOST_CHECK_EQUAL(t4.finalize().lhex(), "dea356a2cddd90c7a7ecedc5ebb563934f460452");
+		a4.update("01234567");
+	auto t4 = a4.finalize();
+	BOOST_CHECK_EQUAL(t4.lhex(), "dea356a2cddd90c7a7ecedc5ebb563934f460452");
+	BOOST_CHECK_EQUAL(t4.uhex(), "DEA356A2CDDD90C7A7ECEDC5EBB563934F460452");
+
+	// Long update.
+	const char* c5 =
+		"abcefghijklmnopqrstuvwxyzABCEFGHIJKLMNOPQRSTUVWXYZ"
+		"abcefghijklmnopqrstuvwxyzABCEFGHIJKLMNOPQRSTUVWXYZ"
+		"abcefghijklmnopqrstuvwxyzABCEFGHIJKLMNOPQRSTUVWXYZ"
+		"abcefghijklmnopqrstuvwxyzABCEFGHIJKLMNOPQRSTUVWXYZ";
+	auto t5 = sha1().update(c5).finalize();
+	BOOST_CHECK_EQUAL(t5.lhex(), "6da7df1af35624ade5dfd2b63a2307d5f283bee4");
+	BOOST_CHECK_EQUAL(t5.uhex(), "6DA7DF1AF35624ADE5DFD2B63A2307D5F283BEE4");
 }

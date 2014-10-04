b144b37f52947b37a024c2256b8b6f1f9307104b
Fabian Schuiki <fschuiki@student.ethz.ch>
2014-10-04 16:32:56 +0200

    improved cov of serialization
    Improved coverage of encodeConversion() and decodeConversion() functions.

--- test/serialization.cpp
+++ test/serialization.cpp
@@ -6,7 +6,7 @@
 #include <boost/test/unit_test.hpp>
 using namespace maxwell::serialization;
 
-BOOST_AUTO_TEST_CASE(endianness) {
+BOOST_AUTO_TEST_CASE(endianness_unsigned) {
 	std::vector<Byte> data;
 	data.reserve(128);
 
@@ -49,12 +49,60 @@ BOOST_AUTO_TEST_CASE(endianness) {
 	dec(a)(b)(c)(d);
 
 	// Make sure the result is native host byte order.
-	BOOST_CHECK_EQUAL(a, 0xab);
-	BOOST_CHECK_EQUAL(b, 0xabcd);
-	BOOST_CHECK_EQUAL(c, 0xabcdef01);
-	BOOST_CHECK_EQUAL(d, 0xabcdef0123456789);
+	BOOST_CHECK_EQUAL(a, (uint8_t)0xab);
+	BOOST_CHECK_EQUAL(b, (uint16_t)0xabcd);
+	BOOST_CHECK_EQUAL(c, (uint32_t)0xabcdef01);
+	BOOST_CHECK_EQUAL(d, (uint64_t)0xabcdef0123456789);
 }
 
+BOOST_AUTO_TEST_CASE(endianness_signed) {
+	std::vector<Byte> data;
+	data.reserve(128);
+
+	// Encode a few integers.
+	VectorEncoder venc(data);
+	Encoder& enc = venc;
+
+	int8_t a = 0xab;
+	int16_t b = 0xabcd;
+	int32_t c = 0xabcdef01;
+	int64_t d = 0xabcdef0123456789;
+	enc(a)(b)(c)(d);
+
+	BOOST_CHECK_EQUAL(data.size(), 15);
+
+	// Make sure the result is big endian (wire format).
+	BOOST_CHECK_EQUAL(data[0],  0xab);
+
+	BOOST_CHECK_EQUAL(data[1],  0xab);
+	BOOST_CHECK_EQUAL(data[2],  0xcd);
+
+	BOOST_CHECK_EQUAL(data[3],  0xab);
+	BOOST_CHECK_EQUAL(data[4],  0xcd);
+	BOOST_CHECK_EQUAL(data[5],  0xef);
+	BOOST_CHECK_EQUAL(data[6],  0x01);
+
+	BOOST_CHECK_EQUAL(data[7],  0xab);
+	BOOST_CHECK_EQUAL(data[8],  0xcd);
+	BOOST_CHECK_EQUAL(data[9],  0xef);
+	BOOST_CHECK_EQUAL(data[10], 0x01);
+	BOOST_CHECK_EQUAL(data[11], 0x23);
+	BOOST_CHECK_EQUAL(data[12], 0x45);
+	BOOST_CHECK_EQUAL(data[13], 0x67);
+	BOOST_CHECK_EQUAL(data[14], 0x89);
+
+	// Decode the integers back.
+	ArrayDecoder adec(&data[0], data.size());
+	Decoder& dec = adec;
+
+	dec(a)(b)(c)(d);
+
+	// Make sure the result is native host byte order.
+	BOOST_CHECK_EQUAL(a, (int8_t)0xab);
+	BOOST_CHECK_EQUAL(b, (int16_t)0xabcd);
+	BOOST_CHECK_EQUAL(c, (int32_t)0xabcdef01);
+	BOOST_CHECK_EQUAL(d, (int64_t)0xabcdef0123456789);
+}
 
 BOOST_AUTO_TEST_CASE(strings) {
 	std::vector<Byte> data;

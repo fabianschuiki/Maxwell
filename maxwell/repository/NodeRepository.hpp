7a5fa580dbb3a3ac1bb9e1ebc12693486f764d13
Fabian Schuiki <fabian@schuiki.ch>
2014-11-14 11:10:29 +0100

    typo fix

--- maxwell/repository/NodeRepository.hpp
+++ maxwell/repository/NodeRepository.hpp
@@ -24,7 +24,7 @@ namespace repository {
 /// across all source files (the node ID). The repository operates on an
 /// abstract node interface which needs to provide the information listed above.
 /// Thus it has no dependency on a specific node class, but can be made to deal
-/// with any class
+/// with any class.
 ///
 /// The repository allows nodes to be added, removed, and looked up. It provides
 /// means to detect whether a source file or node have been modified. Source

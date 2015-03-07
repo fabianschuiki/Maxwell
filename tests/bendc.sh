1cd35fe069ccbee2a108281e1bdecc07094bf5fc
Fabian Schuiki <fabian@schuiki.ch>
2015-03-07 13:53:58 +0100

    test: added backend mw test files
    Added two files to tests/bendc and additional test targets that take a Maxwell
    file and perform the full compile chain on it: Parsing, stage processing,
    backend, backend packaging, and the C compiler. The test fails if any of these
    steps fails.

---
+++ tests/bendc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Copyright (c) 2015 Fabian Schuiki
+# Parses Maxwell files, runs them through the compiler stages, and performs the
+# backend operations on them.
+
+set -e
+rm -rf mwcrepo
+NODES=$("@CMAKE_BINARY_DIR@/maxwell/test/mwcparse" $@ | tail -n 1)
+"@CMAKE_BINARY_DIR@/maxwell/test/mwcstage" $NODES
+FRAGMENTS=$("@CMAKE_BINARY_DIR@/maxwell/test/mwcbendc" $NODES)
+FILES=$("@CMAKE_BINARY_DIR@/maxwell/test/mwcbendcpkg" $FRAGMENTS | grep .c$)
+@CMAKE_C_COMPILER@ $FILES

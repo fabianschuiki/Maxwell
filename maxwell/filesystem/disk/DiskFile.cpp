ef8450ae609f977504f48027eb5464cd9736ab09
Fabian Schuiki <fabian@schuiki.ch>
2015-06-24 12:20:48 +0200

    Throw exception if file I/O fails
    Throw an exception in the DiskFile read() and write() functions if the
    file cannot be read or written. The functions silently fail at the
    moment.
    
    * Add throw statements to DiskFile

--- maxwell/filesystem/disk/DiskFile.cpp
+++ maxwell/filesystem/disk/DiskFile.cpp
@@ -1,6 +1,7 @@
 /* Copyright (c) 2014 Fabian Schuiki */
 #include "maxwell/filesystem/disk/DiskFile.hpp"
 #include <boost/filesystem/fstream.hpp>
+#include <stdexcept>
 using namespace maxwell::filesystem;
 
 
@@ -20,6 +21,8 @@ void DiskFile::read(std::vector<Byte>& dst) const {
 		dst.resize(fin.tellg());
 		fin.seekg(0, std::ios_base::beg);
 		fin.read((char*)&dst[0], dst.size());
+	} else {
+		throw std::runtime_error("file cannot be read");
 	}
 }
 
@@ -27,6 +30,8 @@ void DiskFile::write(const Buffer<const Byte>& src) {
 	boost::filesystem::ofstream fout(path, std::ios_base::binary | std::ios_base::trunc);
 	if (fout.good()) {
 		fout.write((const char*)src.getStart(), src.getLength());
+	} else {
+		throw std::runtime_error("file cannot be written");
 	}
 }
 

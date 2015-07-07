31d55fc6fae71889a23da71642a2af439bc19841
Fabian Schuiki <fabian@schuiki.ch>
2015-07-07 23:04:37 +0200

    Add range class
    Define a maxwell::range class that represents a slice or range of
    elements within a container as a pair of iterators. This simplifies the
    passing around of iterators, especially as a return value of a function.
    Instead of returning a const ref to the container, member functions may
    return a range covering said container's elements, making code that uses
    said function much more container type agnostic.
    
    * Add maxwell/range.hpp

---
+++ maxwell/range.hpp
@@ -0,0 +1,54 @@
+/* Copyright (c) 2015 Fabian Schuiki */
+#pragma once
+#include <iterator>
+
+namespace maxwell {
+
+
+/// A sequence of elements as defined by two iterators.
+/// \needsdoc{members}
+template<class Iterator>
+class range {
+public:
+	typedef range self;
+	typedef Iterator iterator_type;
+	typedef typename std::iterator_traits<Iterator>::difference_type distance_type;
+
+	range() {}
+	range(iterator_type first, iterator_type last): first(first), last(last) {}
+
+	iterator_type begin() const { return first; }
+	iterator_type end() const { return last; }
+
+	distance_type distance() {
+		return std::distance(first,last);
+	}
+
+private:
+	iterator_type first;
+	iterator_type last;
+};
+
+
+/// Creates and returns a range from \a first until and excluding \a last.
+template<class Iterator>
+range<Iterator> make_range(Iterator first, Iterator last) {
+	return range<Iterator>(first, last);
+}
+
+
+/// Creates and returns a range over the container \a c.
+template<class Container>
+auto make_range(Container& c) -> range<decltype(std::begin(c))> {
+	return range<decltype(std::begin(c))>(std::begin(c), std::end(c));
+}
+
+
+/// Calls function \a fn for each element in range \a rng.
+template<class Iterator, class Function>
+Function for_each(range<Iterator> rng, Function fn) {
+	return std::for_each(rng.begin(), rng.end(), fn);
+}
+
+
+} // namespace maxwell
